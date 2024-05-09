9e05cb38107e370a4d00fb3b73b48e5ea89ad537
Bhavik Thanki <bhavik.t@simformsolutions.com>
2024-05-09 19:06:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        "If You Don't Like Your Destiny Don't Accept it, Instead Have The Courage
+        To Change It The Way You Want It To Be"
+      </h1>
+      {console.log(import.meta.env)}
+      {/* <h1>{import.meta.env.VITE_APP_NAME}</h1> */}
+    </>
+  );
+}
+
+export default App;
